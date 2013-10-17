563b93290b3b6d7b5e54b1beead27ce79c22a003
Morgan Prior <morgan@amaras-tech.co.uk>
2013-10-17 20:09:37 +0100

    Initial Files

---
+++ run_exercise_fixedpoint.m
@@ -0,0 +1,29 @@
+%% Matlab Script to *exercise the Simulink Fixed-point Licence
+%% * Not exercising your license will cause you to have them taken away 
+
+%% *nix command line execute with :
+%% $ module load matlab/r2009b
+%% $ matlab -r "run_exercise_fixedpoint;exit" -nodesktop
+
+%% Try catch, to **stop** lic checkout failure halting execution. ie run quit/exit statement.
+a_fixdt = fixdt(1,32,16);
+try 
+  %%   Decide which version of model to run based on Matlab/Simulink Version to avoid warnings.
+  matlab_release = version('-release');
+  switch matlab_release
+      case '2009b'
+          sim(  'exercise_fixedpoint_2009b' );
+      case '2012b'
+          sim(  'exercise_fixedpoint' ); % Uses .slx
+      otherwise
+          disp( 'Exact version match not created, Falling back to a 2008b version' );
+          sim(  'exercise_fixedpoint_2008b' );
+  end
+catch
+  disp('Caught Error : probably no fixed point license for you today')
+end
+
+%% Only call quit/exit if called from the command line
+%quit
+
+
