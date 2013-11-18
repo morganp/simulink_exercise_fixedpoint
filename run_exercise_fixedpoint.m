2fc7848277bde91567261fdc3bfb4a44df1657f5
Morgan Prior <morgan@amaras-tech.co.uk>
2013-11-18 08:12:36 +0000

    Check model runs first before deciding error was due to fixedpoint.

--- run_exercise_fixedpoint.m
+++ run_exercise_fixedpoint.m
@@ -5,6 +5,25 @@
 %% $ module load matlab/r2009b
 %% $ matlab -r "run_exercise_fixedpoint;exit" -nodesktop
 
+%% Run model in integer mode to check it actually works
+a_fixdt = 'int32';
+try 
+  %%   Decide which version of model to run based on Matlab/Simulink Version to avoid warnings.
+  matlab_release = version('-release');
+  switch matlab_release
+      case '2009b'
+          sim(  'exercise_fixedpoint_2009b' );
+      case '2012b'
+          sim(  'exercise_fixedpoint' ); % Uses .slx
+      otherwise
+          disp( 'Exact version match not created, Falling back to a 2008b version' );
+          sim(  'exercise_fixedpoint_2008b' );
+  end
+  disp('Model ran in integer mode.')
+catch
+  disp('Caught Error : Can not run model')
+end
+
 %% Try catch, to **stop** lic checkout failure halting execution. ie run quit/exit statement.
 a_fixdt = fixdt(1,32,16);
 try 
